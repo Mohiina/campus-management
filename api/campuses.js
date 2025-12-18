25db2accab30e1cb5f5a52571c1e189a0c3a857e
Mohiina <mohiinau@gmail.com>
2025-12-18 18:36:48 -0500

    Create API routes folder and initial routes files

---
+++ api/campuses.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { Campus, Student } = require('../db/models');
+
+/**
+ * GET /api/campuses
+ * Returns all campuses
+ */
+router.get('/', async (req, res, next) => {
+  try {
+    const campuses = await Campus.findAll();
+    res.json(campuses);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * GET /api/campuses/:campusId
+ * Returns one campus + its students
+ */
+router.get('/:campusId', async (req, res, next) => {
+  try {
+    const campus = await Campus.findByPk(req.params.campusId, {
+      include: Student
+    });
+
+    if (!campus) {
+      res.status(404).send('Campus not found');
+    } else {
+      res.json(campus);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
---
+++ api/students.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { Student, Campus } = require('../db/models');
+
+/**
+ * GET /api/students
+ * Returns all students
+ */
+router.get('/', async (req, res, next) => {
+  try {
+    const students = await Student.findAll();
+    res.json(students);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * GET /api/students/:studentId
+ * Returns one student + their campus
+ */
+router.get('/:studentId', async (req, res, next) => {
+  try {
+    const student = await Student.findByPk(req.params.studentId, {
+      include: Campus
+    });
+
+    if (!student) {
+      res.status(404).send('Student not found');
+    } else {
+      res.json(student);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
