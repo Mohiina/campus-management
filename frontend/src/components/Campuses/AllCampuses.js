c449ae0592f42522691a92b2c26b09694f079d72
Mohiina <mohiinau@gmail.com>
2025-12-21 04:52:56 -0500

    Setup React Router and placeholder pages for campuses and students

---
+++ frontend/src/components/Campuses/AllCampuses.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AllCampuses = () => {
+  return <h2>All Campuses Page</h2>;
+};
+
+export default AllCampuses;
---
+++ frontend/src/components/Students/AllStudents.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AllStudents = () => {
+  return <h2>All Students Page</h2>;
+};
+
+export default AllStudents;
